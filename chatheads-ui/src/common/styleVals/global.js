c826d61e0e2e70f681d6a2dbb5dba7d9668ad21d
Shreyash Padhi <shreyashpadhi101@gmail.com>
2020-07-12 01:30:41 +0530

    added font options and dimensions

--- chatheads-ui/src/common/styleVals/global.js
+++ chatheads-ui/src/common/styleVals/global.js
@@ -8,13 +8,18 @@ export default {
         spacing10: '10px',
         spacing12: '12px',
         spacing14: '14px',
-        spacing16: '16px'
+        spacing16: '16px',
+        spacing18: '18px',
+        spacing20: '20px',
+        spacing22: '22px'
     },
     color: {
         dark: '#262926',
-        light: '#dbdad9'
+        light: '#dbdad9',
+        orange: '#fd7e14'
     },
     fonts: {
-        segoeUI: 'Segoe UI Light'
+        segoeUI: 'Segoe UI Light',
+        wordFont: 'Ubuntu, sans-serif'
     }
 }
